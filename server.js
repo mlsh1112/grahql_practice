11881d43d1c35a99338d49c0539a38f0253158c3
이서현 <seohyun.lee@lotte.net>
2023-02-10 16:33:26 +0900

    Add movie api

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 import { ApolloServer, gql } from "apollo-server";
-
+import fetch from "node-fetch";
 
 let tweets = [
     {
         id: '1',
         text: 'first one',
+        userId: '2'
     },
     {
         id: '2',
         text: 'second one',
+        userId: '1'
+    }
+]
+
+let users = [
+    {
+        id: '1',
+        firstName : 'suzy',
+        lastName: 'kim'
+    },
+    {
+        id: '2',
+        firstName : 'sua',
+        lastName: 'lee'
     }
 ]
 
@@ -17,8 +32,15 @@ const typeDefs = gql`
         id: ID!,
         username : String!,
         firstName : String!,
-        lastName: String
+        lastName: String,
+        """
+        Is the sum of firstName + lastName as a string
+        """
+        fullName: String!
     }
+    """
+    TweetTweet
+    """
     type Tweet {
         id: ID!,
         text: String!,
@@ -26,25 +48,69 @@ const typeDefs = gql`
     }
 
     type Query {
+        allUsers : [User!]!
         allTweets : [Tweet!]!
+        allMovies : [Movie!]!
         tweet(id:ID!): Tweet
+        movie(id:ID!): Movie
     }
 
     type Mutation {
         postTweet(text: String!, userId:ID!): Tweet!
+        """
+        Deletes a Tweet if found, else return false
+        """
         deleteTweet(id:ID!) : Boolean!
+    },
+    type Movie {
+        id: Int!
+        url: String!
+        imdb_code: String!
+        title: String!
+        title_english: String!
+        title_long: String!
+        slug: String!
+        year: Int!
+        rating: Float!
+        runtime: Float!
+        genres: [String]!
+        summary: String
+        description_full: String!
+        synopsis: String
+        yt_trailer_code: String!
+        language: String!
+        background_image: String!
+        background_image_original: String!
+        small_cover_image: String!
+        medium_cover_image: String!
+        large_cover_image: String!
     }
+
 `;
 // POST, PUT, DELETE => Mutation
 // required : !
 
 const resolvers = {
     Query:{
+        allUsers(){
+            return users;
+        },
         allTweets(){
             return tweets;
         },
+        allMovies(){
+            return fetch("https://yts.mx/api/v2/list_movies.json")
+            .then(res=> res.json())
+            .then(json=> json.data.movies)
+        },
         tweet(root, {id}){
             return tweets.find((tweet)=> tweet.id === id);
+        },
+        movie(_, {id}){
+            console.log(id)
+            return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+            .then(res=> res.json())
+            .then(json=> json.data.movie)
         }
     },
     Mutation:{
@@ -62,6 +128,17 @@ const resolvers = {
             if(!tweet) return false;
             tweets = tweets.filter(tweet => tweet.id !== id);
             return true
+        },
+
+    },
+    User : {
+        fullName({firstName, lastName}){
+            return `${firstName} ${lastName}`;
+        }
+    },
+    Tweet : {
+        author({userId}){
+            return users.find(user => userId === user.id)
         }
     }
 }
